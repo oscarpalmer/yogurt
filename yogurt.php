39e965d4e7dd004df21646d26de7af2c0394551a
Oscar Palmer <opalmer@me.com>
2013-05-19 18:36:43 +0200

    Silly little mistake. Strpos needs two parameters, Oscar.

--- yogurt.php
+++ yogurt.php
@@ -102,7 +102,7 @@ class Yogurt {
         else {
           $key = self::dotkey_to_objkey($operator[1]);
           $opr = ($operator[2] == "is" || $operator[2] == "==") ? "==" : "!=";
-          $val = strpos($operator[3]) === 0 ? self::dotkey_to_objkey($operator[3]) : $operator[3];
+          $val = strpos($operator[3], "$") === 0 ? self::dotkey_to_objkey($operator[3]) : $operator[3];
           $blk = $name == "if" ? $operator[4] : "";
 
           $template = str_replace($match, "<?php $open ($key $opr $val): ?>$blk$close", $template);
