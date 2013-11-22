2385478f9a67a5069abeb190215b99d6f52391b0
Oscar Palmér <opalmer@me.com>
2013-11-22 19:50:49 +0100

    0.3.0
    Syntax changes both in template and test file. Most notably, no more double braces.

--- test/index.php
+++ test/index.php
@@ -1,24 +1,26 @@
 <?php
 
+# Root.
 $root = dirname(__FILE__);
 
-//
-
+# Yogurt.
 require_once "$root/../yogurt.php";
 
-//
+# Uncomment to check plain output.
+#header("Content-Type: text/plain");
 
-$vars = array(
+# Variables.
+$vars = [
   "title"   => "Yogurt",
-  "tag"     => "This is Yogurt, a tiny templating engine for Dagger, Lana, &amp; others.",
+  "tag"     => "This is Yogurt, a tiny templating engine for Dagger &amp; others.",
   "body"    => "<p>This is regular HTML.</p>",
   "partial" => "_partial.html",
-  "array"   => array(array("title" => "#1", "body" => "This is the body for #1."),
-                   array("title" => "#2", "body" => "This is the body for #2."),
-                   array("title" => "#3", "body" => "This is the body for #3.")));
+  "array"   => [
+    ["title" => "#1", "body" => "This is the body for #1."],
+    ["title" => "#2", "body" => "This is the body for #2."],
+    ["title" => "#3", "body" => "This is the body for #3."]
+  ]
+];
 
-//
-
-$yogurt = new Yogurt();
-
-echo $yogurt::render(file_get_contents("$root/template.html"), $vars);
+# Render and echo.
+echo Yogurt::render(file_get_contents("$root/template.html"), $vars);
