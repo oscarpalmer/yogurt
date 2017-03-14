215177f3d08d667b8eb357c728b8ae7dbf60e504
Oscar Palmér <opalmer@me.com>
2017-03-14 18:34:41 +0100

    proper namespaces
    added test for data object

--- tests/oscarpalmer/Yogurt/Test/YogurtTest.php
+++ tests/oscarpalmer/Yogurt/Test/YogurtTest.php
@@ -6,7 +6,7 @@ use oscarpalmer\Yogurt\Dairy;
 use oscarpalmer\Yogurt\Flavour;
 use oscarpalmer\Yogurt\Yogurt;
 
-class YogurtTest extends \PHPUnit_Framework_TestCase
+class YogurtTest extends \PHPUnit\Framework\TestCase
 {
     # Mock variables.
     protected $directory;
